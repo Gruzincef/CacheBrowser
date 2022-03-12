44cd3dc2cffcbec61d43471911f7c1a4b3b71e5a
Gruzincef <gruzincef@mail.ru>
2022-03-12 21:05:08 +0700

    Создан класс для работы с потоками

---
+++ Threaders.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Threading;
+
+namespace WorkBrowser
+{
+    /// <summary>
+    /// Description of Threaders.
+    /// </summary>
+    public class Threaders
+    {
+        public Threaders()
+        {
+        }
+        private Thread WorkThreading;
+        public delegate void FunctionThreadNonArg();
+        public delegate void FunctionThreadArg(object ob);
+        public Thread StartThead(FunctionThreadArg functionthreadarg, object ob)
+        {
+            WorkThreading = new Thread(new ParameterizedThreadStart(functionthreadarg));
+            WorkThreading.Start(ob);
+            WorkThreading.IsBackground = true;
+            return WorkThreading;
+        }
+        public Thread StartThead(FunctionThreadNonArg functionthreadnonarg)
+        {
+            WorkThreading = new Thread(new ThreadStart(functionthreadnonarg));
+            WorkThreading.Start();
+            WorkThreading.IsBackground = true;
+            return WorkThreading;
+        }
+        public void AbortThead(int tm)
+        {
+            if (WorkThreading.IsAlive) WorkThreading.Abort(tm);
+        }
+        public Thread GetTheard()
+        {
+            return WorkThreading;
+        }
+
+    }
+}
